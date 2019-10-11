2838020e13f963ca70d4a0e6dc0d7940a10e9256
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-10-11 12:38:52 -0400

    virtual topo, create random graph

--- src/core/VirtualTopo.py
+++ src/core/VirtualTopo.py
@@ -1,11 +1,25 @@
 import random
 from namedlist import namedlist
+from collections import deque
+from math import ceil
 import graphviz
-Connection=namedlist("Connection",["node","weight"])
+class Connection(namedlist("Connection",["node","weight"])):
+	def __hash__(self):
+		return hash((self.node,self.weight))
 class Node:
 	def __init__(self,name):
 		self.name=name
 		self.children={}
+	def traverse(self):
+		unvisited=deque([self])
+		ans=set()
+		while unvisited:
+			u=unvisited.pop()
+			ans.add(u)
+			for children in u:
+				if children.node not in ans:
+					unvisited.append(children.node)
+		return ans
 	def __iadd__(self,node):
 		assert isinstance(node,Node)
 
@@ -17,34 +31,57 @@ class Node:
 		return iter(self.children.values())
 	def __str__(self):
 		return self.name
+	def __repr__(self):
+		return f"Node({self.name})"
 	def __hash__(self):
 		return hash(str(self))
 	def has_children(self):
 		return bool(self.children)
 class VirtualTopo:
-	def __init__(self,n,nodes_prefix="v",min_weight=1,max_weight=1000):
+	def __init__(self,n,nodes_prefix="v",volume=None,min_weight=1,max_weight=1000):
+		"""Create random undirected graph  with n nodes
+		Args:
+			n (int): Number of nodes
+			nodes_prefix (str,optional): prefix of the name of each node, default='v' 
+			volume (int,optional): number indicating how many connection in the whole graph,
+				must be in (0,1), default:random.
+			min_weight (int,optional) minimal value of the weight
+			max_weight (int,optional) maximum value of the weight
+
+		"""
 		self.nodes=[Node(f"{nodes_prefix}{i+1}") for i in range(n)]
+		volume=random.random() if not volume else volume
 		max_connections=(n*(n-1))//2
-		no_connections=random.randint(1,max_connections)
-		pairs=set()
+		no_connections=ceil(max_connections*volume)
+		self.pairs=set()
 		random_index=lambda :random.randrange(n)
-		while len(pairs)!=no_connections:
+		while len(self.pairs)!=no_connections:
 			i,j=random_index(),random_index()
 			if i==j:
 				continue
-			pairs.add(frozenset((i,j)))
-		for i,j in pairs:
+			self.pairs.add(frozenset((i,j)))
+		for i,j in self.pairs:
 			u,v=self.nodes[i],self.nodes[j]
-			print(u,v)
 			u+=v
 			v+=u
-		#connect outliers
-		not_outlier_index,not_outlier=next(((i,u) for i,u in enumerate(self.nodes) if u.has_children()))
-		for i,u in enumerate(self.nodes):
-			if not u.has_children():
-				pairs.add(frozenset((i,not_outlier_index)))
-				u+=not_outlier
-				not_outlier+=u
+		#connect cycles if any
+		cycles=set()
+		for u in self.nodes:
+			cycles.add(frozenset(u.traverse()))
+		#if there is more than 1 cycle then join them
+		it=iter(cycles)
+		first_cycle=next(it)
+		u=next(iter(first_cycle))
+		i=self.nodes.index(u)
+		while True:
+			to_join=next(it,None)
+			if not to_join:
+				break
+			v=next(iter(to_join))
+			j=self.nodes.index(v)
+			self.pairs.add(frozenset((i,j)))
+			u+=v
+			v+=u
 		#set random weights
 		for node in self.nodes:
 			for conn in node:
@@ -52,14 +89,23 @@ class VirtualTopo:
 					a=random.randint(min_weight,max_weight)
 					conn.weight=a
 					conn.node[node].weight=a
-	def to_graphviz(self,*args,**kwargs):
+		self.graphviz=self.__to_graphviz()
+	def view(self,name="virtual_topo"):
+		"""opens default pdf binary to show the graph, and saves as pdf
+		Args:
+			name(str): pdf file's name to save, default "virtual_topo"
+		"""
+		self.graphviz.view(name,cleanup=True,quiet_view=True)
+	def savefig(self):
+		pass
+	def __to_graphviz(self,*args,**kwargs):
 		graph=graphviz.Graph(*args,**kwargs)
-		sid=lambda obj:str(id(obj))
-		for node in self.nodes:
-			graph.node(sid(node),label=node.name)
-			for conn in node:
-				graph.edge(sid(node),sid(conn.node),label=str(conn.weight))
+		for i,j in self.pairs:
+			u,v=self.nodes[i],self.nodes[j]
+			assert u[v].weight==v[u].weight
+			graph.edge(u.name,v.name,label=str(u[v].weight))
 		return graph
 if __name__=="__main__":
-	topo=VirtualTopo(5)
-	topo.to_graphviz(format="pdf").render("tree.dot")
+	topo=VirtualTopo(7,volume=.75)
+	topo.view()
+	
